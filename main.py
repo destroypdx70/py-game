789b07876049e1af25af7a24931c19e85d4cc71c
destroypdx70 <destroypdx7@protonmail.com>
2023-03-13 00:43:37 -0500

    Animation of weapon and sound

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from player import *
 from raycasting import *
 from object_renderer import *
 from sprite_object import *
+from object_handler import *
+from weapon import *
+from sound import *
 
 class Game:
     def __init__(self):
@@ -21,21 +24,24 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-        self.static_sprite = SpriteObject(self)
-        self.animated_sprite = AnimatedSprites(self)
+        # self.static_sprite = SpriteObject(self)
+        # self.animated_sprite = AnimatedSprite(self)
+        self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
+        self.sound = Sound(self)
 
     def update(self):
         self.player.update()
         self.raycasting.update()
-        self.static_sprite.update()
-        self.animated_sprite.update()
+        self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f"{self.clock.get_fps() :.1f}")
 
     def draw(self):
-        # self.screen.fill("black")
         self.object_renderer.draw()
+        self.weapon.draw()
         # self.map.draw()
         # self.player.draw()
 
@@ -44,6 +50,7 @@ class Game:
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            self.player.single_fire_event(event)
 
     def run(self):
         while True:
